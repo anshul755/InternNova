7183016edcec5bbfce855444b28a1d8c3d2a012f
anshul755 <anshulpatel2023@gmail.com>
2025-12-11 14:28:55 +0530

    Initial commit

---
+++ Backend/src/test/java/com/internNova/InternNova/InternNovaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.internNova.InternNova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternNovaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
